a67b21efa2fd961bca4d3165da8ae661934bfeae
Kuldeep7devi <kushallaxkar9@gmail.com>
2026-03-24 14:59:29 +0530

    i make a comment

--- src/Component/BasicComponent/ToggleButton.jsx
+++ src/Component/BasicComponent/ToggleButton.jsx
@@ -5,6 +5,7 @@ const ToggleButton = ({ isOpen, onToggle, openText, closeText }) => {
             className="bg-purple-600 text-white p-3 rounded-sm w-fit
                  transition active:scale-95 flex items-center gap-2"
         >
+            {/* make comment  */}
             {isOpen ? closeText : openText}
         </button>
     )
